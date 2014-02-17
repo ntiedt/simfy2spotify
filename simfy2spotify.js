8106cdc0d087e37c3d61730b993af86650171a4e
Niels Tiedt <cool_coders@web.de>
2014-02-17 12:15:32 +0100

    Version 1.2
    - Tested in Firefox 27.0.1
    - Fixed some bugs
    - Implement language object
    - Allways build dropdown with found track/album list with preselected
    result if found.
    - Swapped out link building in new function
    - Style adjustments

--- simfy2spotify.js
+++ simfy2spotify.js
@@ -37,7 +37,7 @@ javascript:
       var cssbookmarklet = document.createElement('link');
       cssbookmarklet.type = 'text/css';
       cssbookmarklet.rel = 'stylesheet';
-      cssbookmarklet.href = 'http://www.typo3-coder.org/simfy2spotify/simfy2spotify.css?timestamp='+timestamp;
+      cssbookmarklet.href = 'http://ntiedt.github.io/simfy2spotify/simfy2spotify.css?timestamp='+timestamp;
       document.getElementsByTagName('head')[0].appendChild(cssbookmarklet); 
     }
     function initsimfy2spotify() {
@@ -45,14 +45,40 @@ javascript:
         var tracks = [];
         // Get language for territory restriction
         var lang = navigator.language.slice(0,2).toUpperCase();
+        var ll = {
+          tableHeader1: 'Playlist:',
+          tableHeader2: 'Album:',
+          tableHeader3: 'Artist-Name:',
+          tableHeader4: 'Track-Title:',
+          tableHeader5: 'Length:',
+          tableHeader6: 'State:',
+          tableHeader7: 'Results:',
+          tableHeader8: 'Length:',
+          tableHeader9: 'Spotify-Links:',
+          btCheckAll: 'Check All',
+          btClose: 'simfy2spotify closes and u must reload the page if you want to reuse it!',
+          buildPlaylist: 'Build playlist',
+          select1: 'Please select!',
+          noResultFound: 'No result found!',
+          found: 'Found',
+          foundLength: 'Found with different length',
+          noResultFoundButWithOtherLength1: 'No result found with specified length!<br/>Too much results(',
+          noResultFoundButWithOtherLength2: ') with different length found!',
+          tooMuch1: 'Too much results with same length(',
+          tooMuch2: ')!',
+          errorNoSpotifyLinks: 'Error! No spotify links could be found! Please verify your inputs and recheck!',
+          errorNotAllSpotifyLinks1: 'Warning! Could not extract all(',
+          errorNotAllSpotifyLinks2: ') spotify link from all('+num_rows+') rows!',
+          copyC: 'After closing this message press CTRL + C to copy your playlist!'
+        }
         
         /**
          * Extract simfy playlist and build table with buttons
          */
         function extractSimfy() {
-          var titlebar = '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><span id="ui-id-1" class="ui-dialog-title">simfy2spotify</span><div class=""><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-close" role="button" aria-disabled="false" title="close"><span class="ui-button-icon-primary ui-icon ui-icon-close"></span><span class="ui-button-text">close</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-dialog-titlebar-maximize" role="button" aria-disabled="false" title="maximize"><span class="ui-button-text">&#9723;</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-dialog-titlebar-minimize" role="button" aria-disabled="false" title="minimize"><span class="ui-button-text">_</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-reload" role="button" aria-disabled="false" title="reload"><span class="ui-button-icon-primary ui-icon ui-icon-refresh"></span><span class="ui-button-text">reload</span></button></div></div>';
-          var simfyPlaylistOut = '<table class="simfy-playlist ui-widget ui-widget-content"><thead><tr class="ui-widget-header"><th>Playlist:</th><th>Album:</th><th>Artist-Name:</th><th>Track-Title:</th><th>Length:</th><th>State:</th><th>Spotify-Links:</th></tr></thead><tbody>';
-          simfyPlaylistOut += '<tfoot><tr class="all"><td class="playlist_title"></td><td class="album"></td><td class="artist_name"></td><td class="track_title"></td><td class="length"></td><td><a href="" class="spot-tracks notchecked">Check All</a><a href="" class="build-playlist notchecked">Build playlist</a></td><td class="spotify"></td></tr></tfoot>';
+          var titlebar = '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix"><span id="ui-id-1" class="ui-dialog-title"><a href="http://ntiedt.github.io/simfy2spotify/" target="_blank" class="simfy2spotify-home">simfy2spotify</a></span><div class=""><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-close" role="button" aria-disabled="false" title="close"><span class="ui-button-icon-primary ui-icon ui-icon-close"></span><span class="ui-button-text">close</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-dialog-titlebar-maximize" role="button" aria-disabled="false" title="maximize"><span class="ui-button-text">&#9723;</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-dialog-titlebar-minimize" role="button" aria-disabled="false" title="minimize"><span class="ui-button-text">_</span></button><button type="button" class="ui-button ui-widget ui-state-default ui-corner-all ui-button-icon-only ui-dialog-titlebar-reload" role="button" aria-disabled="false" title="reload"><span class="ui-button-icon-primary ui-icon ui-icon-refresh"></span><span class="ui-button-text">reload</span></button></div></div>';
+          var simfyPlaylistOut = '<table class="simfy-playlist ui-widget ui-widget-content"><thead><tr class="ui-widget-header"><th>'+ll.tableHeader1+'</th><th>'+ll.tableHeader2+'</th><th>'+ll.tableHeader3+'</th><th>'+ll.tableHeader4+'</th><th>'+ll.tableHeader5+'</th><th>'+ll.tableHeader6+'</th><th>'+ll.tableHeader7+'</th><th>'+ll.tableHeader8+'</th></tr></thead><tbody>';
+          simfyPlaylistOut += '<tfoot><tr class="all"><td class="playlist_title"></td><td class="album"></td><td class="artist_name"></td><td class="track_title"></td><td class="length"></td><td><a href="" class="spot-tracks notchecked">'+ll.btCheckAll+'</a><a href="" class="build-playlist notchecked">'+ll.buildPlaylist+'</a></td><td class="results"></td><td class="spotify"></td></tr></tfoot>';
           playlist_title = $('h2.playlist_title').text();
           if(playlist_title.lastIndexOf(' - ')!=-1){
             album = playlist_title.slice(playlist_title.lastIndexOf(' - ')+3);
@@ -63,7 +89,7 @@ javascript:
             artist_name = $('.artist_name a', this).attr('title');
             track_title = $('.track_title_and_version_title .title', this).attr('title');
             length = $('.length', this).text();
-            simfyPlaylistOut += '<tr id="row_'+nr+'"><td class="playlist_title"><input value="'+playlist_title+'" /></td><td class="album"><input value="'+album+'" /></td><td class="artist_name"><input value="'+artist_name+'" /></td><td class="track_title"><input value="'+track_title+'" /></td><td class="length"><input value="'+length+'" /></td><td><a href="" class="spot-track notchecked" data-row="row_'+nr+'">Check</a></td><td class="spotify"></td></tr>';
+            simfyPlaylistOut += '<tr id="row_'+nr+'"><td class="playlist_title"><input value="'+playlist_title+'" /></td><td class="album"><input value="'+album+'" /></td><td class="artist_name"><input value="'+artist_name+'" /></td><td class="track_title"><input value="'+track_title+'" /></td><td class="length"><input value="'+length+'" /></td><td><a href="" class="spot-track notchecked" data-row="row_'+nr+'">Check</a></td><td class="results"></td><td class="spotify"></td></tr>';
           });
           simfyPlaylistOut += '</tbody></table>';
           var simfyResult = $('<div>')
@@ -114,16 +140,22 @@ javascript:
          * Build album drop down
          * @param {json} results - found album results
          * @param {object} cur_row
+         * @param {int} resultkey - first found track with correct specified length
          */
-        function buildAlbums(results,cur_row) {
-          cur_row.find('.album').html($( '<select/>', {'class': 'albums'}).html( $('<option/>', {'value': '', 'text': 'Please select album!'}) ));
+        function buildAlbums(results,cur_row,resultkey) {
+          cur_row.find('.results').html($( '<select/>').html( $('<option/>', {'value': '', 'text': ll.select1}) ));
           $.each( results, function( key, track) {
-            $('<option/>', {'value': track.album.name, 'data-length': Math.round(track.length), 'text': track.album.name + ' / Released: ' + track.album.released + ' Length: ' + getTrackTime(Math.round(track.length))}).appendTo( cur_row.find('.album > select.albums') );
-            cur_row.find('.album > select.albums').on('change',function(){
+            if(resultkey==key){
+              $('<option/>', {'selected': 'selected', 'value': track.album.name, 'data-length': Math.round(track.length), 'data-album-href': track.album.href, 'data-track-href': track.href, 'data-artist-href': track.artists[0].href, 'text': track.album.name + ' / Released: ' + track.album.released + ' Length: ' + getTrackTime(Math.round(track.length)) + ' Track-Nr.: ' + track['track-number'] + ' Track-Popularity: ' + track.popularity }).appendTo( cur_row.find('.results > select') );
+            }else{
+              $('<option/>', {'value': track.album.name, 'data-length': Math.round(track.length), 'data-album-href': track.album.href, 'data-track-href': track.href, 'data-artist-href': track.artists[0].href, 'text': track.album.name + ' / Released: ' + track.album.released + ' Length: ' + getTrackTime(Math.round(track.length)) + ' Track-Nr.: ' + track['track-number'] + ' Track-Popularity: ' + track.popularity }).appendTo( cur_row.find('.results > select') );
+            }
+            cur_row.find('.results > select').on('change',function(){
               if($(this).val()!=''){
-                $.getJSON( getUrl($(cur_row)), function( json ) {
-                  extractResults(json,cur_row.find('.spot-track'));
-                });
+                artist = cur_row.find('.results select option:selected').data('artist-href');
+                album = cur_row.find('.results select option:selected').data('album-href');
+                track = cur_row.find('.results select option:selected').data('track-href');
+                buildSpotifyLinksFromOption(artist,album,track,cur_row.find('.spotify'));
               }
             });
           });
@@ -137,13 +169,13 @@ javascript:
         function extractResults(json,cur_a){
           var cur_row = $('#'+cur_a.data('row'));
           var num_results = json.info.num_results;
-          var num_langresults = 0;
-          var num_lengthresults = 0;
-          var resultkey = 0;
+          var num_langresults = -1;
+          var num_lengthresults = -1;
+          var resultkey = -1;
+          var resultkey2 = -1;
           var results = [];
           var resultswithoutlength = [];
-          var length = (cur_row.find('.album select').length>0) ? cur_row.find('.album select option:selected').data('length') : getSeconds(cur_row.find('.length input').val());
-          
+          var length = (cur_row.find('.results select').length>0) ? cur_row.find('.results select option:selected').data('length') : getSeconds(cur_row.find('.length input').val());
           if(num_results==0){
             cur_a.text('Not found').removeClass('notchecked notfound toomuch found').addClass('notfound');
             return;
@@ -154,34 +186,47 @@ javascript:
               resultswithoutlength.push(track);
               if(Math.round(track.length)==length){
                 num_lengthresults++;
-                resultkey = key;
+                if(resultkey==-1) {
+                  resultkey = key;
+                }
+                if(resultkey2==-1) {
+                  resultkey2 = num_langresults;
+                }
                 results.push(track);
               }
             }
           });
-          if(num_lengthresults==0){
-            if(num_langresults==0){
-              cur_a.text('No result found!').removeClass('notchecked notfound toomuch found').addClass('notfound');
+          // No results with corresponding length
+          if(num_lengthresults==-1){
+            // No results with corresponding lang -> No result
+            if(num_langresults==-1){
+              cur_a.text(ll.noResultFound).removeClass('notchecked notfound toomuch found').addClass('notfound');
               return;
+            // Single result with corresponding lang
             }else if(num_langresults==1){
-              cur_a.text('Found').removeClass('notchecked notfound toomuch found').addClass('found');
-              buildSpotifyLinks(json.tracks[resultkey],cur_row.find('.spotify'));
+              cur_a.text(ll.foundLength).removeClass('notchecked notfound toomuch found').addClass('found');
+              buildAlbums(resultswithoutlength,cur_row,resultkey2);
+              buildSpotifyLinks(resultswithoutlength[resultkey2],cur_row.find('.spotify'));
+            // Too much results with corresponding lang
             }else{
-              cur_a.html('No result found!<br/>Too much results('+num_langresults+') with different length found!').removeClass('notchecked notfound toomuch found').addClass('toomuch');
-              buildAlbums(resultswithoutlength,cur_row);
+              cur_a.html(ll.noResultFoundButWithOtherLength1+num_langresults+ll.noResultFoundButWithOtherLength2).removeClass('notchecked notfound toomuch found').addClass('toomuch');
+              buildAlbums(resultswithoutlength,cur_row,-1);
             }
+          // Too much results with corresponding length
           }else if(num_lengthresults>1){
-            cur_a.text('Too much results('+num_lengthresults+')!').removeClass('notchecked notfound toomuch found').addClass('toomuch');
-            buildAlbums(results,cur_row);
+            cur_a.text(ll.tooMuch1+num_lengthresults+ll.tooMuch2).removeClass('notchecked notfound toomuch found').addClass('toomuch');
+            buildAlbums(resultswithoutlength,cur_row,-1);
             return;
+          // Single result with corresponding length
           }else{
-            cur_a.text('Found').removeClass('notchecked notfound toomuch found').addClass('found');
-            buildSpotifyLinks(json.tracks[resultkey],cur_row.find('.spotify'));
+            cur_a.text(ll.found).removeClass('notchecked notfound toomuch found').addClass('found');
+            buildAlbums(resultswithoutlength,cur_row,resultkey2);
+            buildSpotifyLinks(resultswithoutlength[resultkey2],cur_row.find('.spotify'));
           }
         }
         
         /**
-         * Build spotify links
+         * Build spotify links from single json
          * @param {json} track
          * @param {object} target - where to insert
          */
@@ -200,6 +245,32 @@ javascript:
           $( '<a/>', {
             'class': 'track-link',
             'href': track.href,
+            html: 'Track'
+          }).appendTo( target );
+        }
+        
+        /**
+         * Build spotify links from option datas
+         * @param {string} artist
+         * @param {string} album
+         * @param {string} track
+         * @param {object} target - where to insert
+         */
+        function buildSpotifyLinksFromOption(artist,album,track,target) {
+          target.html('');
+          $( '<a/>', {
+            'class': 'artist-link',
+            'href': artist,
+            html: 'Artist'
+          }).appendTo( target );
+          $( '<a/>', {
+            'class': 'album-link',
+            'href': album,
+            html: 'Album'
+          }).appendTo( target );
+          $( '<a/>', {
+            'class': 'track-link',
+            'href': track,
             html: 'Track'
           }).appendTo( target );
         }
@@ -247,7 +318,7 @@ javascript:
         
         // EVENT: Titlebar: Click: close
         $('.simfy-result .ui-dialog-titlebar-close').click(function(e){
-          alert('simfy2spotify closes and u must reload the page if you want to reuse it!');
+          alert(ll.btClose);
           $('.simfy-result').remove();
         });
         
@@ -297,13 +368,13 @@ javascript:
           });
           
           if(num_rows<1){
-            alert('Error! No spotify links could be found!');
+            alert(ll.errorNoSpotifyLinks);
             return;
           }else{
             if(num_tracks<num_rows){
-              alert('Warning! Could not extract all('+num_tracks+') spotify link from all('+num_rows+') rows!');
+              alert(ll.errorNotAllSpotifyLinks1+num_tracks+ll.errorNotAllSpotifyLinks2);
             }
-            alert('After closing this message press CTRL + C to copy your playlist!');
+            alert(ll.copyC);
           }
           $('textarea.spotify-playlist').focus().select();
           
